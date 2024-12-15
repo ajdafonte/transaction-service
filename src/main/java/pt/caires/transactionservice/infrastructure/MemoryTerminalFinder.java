5b1a9dbbf509bffe34af5032d92e45e3a6b84db9
Alejandro Caires <4990971+ajdafonte@users.noreply.github.com>
2024-12-15 19:03:35 +0100

    Improve logging

--- src/main/java/pt/caires/transactionservice/infrastructure/MemoryTerminalFinder.java
+++ src/main/java/pt/caires/transactionservice/infrastructure/MemoryTerminalFinder.java
@@ -3,6 +3,8 @@ package pt.caires.transactionservice.infrastructure;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import pt.caires.transactionservice.domain.terminal.Terminal;
 import pt.caires.transactionservice.domain.terminal.TerminalFinder;
@@ -10,12 +12,16 @@ import pt.caires.transactionservice.domain.terminal.TerminalFinder;
 @Component
 public class MemoryTerminalFinder implements TerminalFinder {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(MemoryTerminalFinder.class);
+
   private static final Set<String> AVAILABLE_TERMINALS = new HashSet<>() {{
     add("e3211be6-d0cc-4718-905d-ab933cc91ecb");
   }};
 
   @Override
   public Optional<Terminal> findBy(String id, int threatScore) {
+    LOGGER.info("Attempt to retrieve terminal {}", id);
+
     return AVAILABLE_TERMINALS.contains(id) ?
         Optional.of(new Terminal(id, threatScore)) :
         Optional.empty();
--- src/main/java/pt/caires/transactionservice/usecase/CreateTransaction.java
+++ src/main/java/pt/caires/transactionservice/usecase/CreateTransaction.java
@@ -1,6 +1,8 @@
 package pt.caires.transactionservice.usecase;
 
 import java.util.function.Function;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import pt.caires.transactionservice.domain.exception.NotFoundServiceException;
 import pt.caires.transactionservice.domain.terminal.Terminal;
@@ -9,6 +11,8 @@ import pt.caires.transactionservice.domain.transaction.TransactionService;
 
 @Service
 public class CreateTransaction {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(CreateTransaction.class);
 
   private final TerminalFinder terminalFinder;
   private final TransactionService transactionService;
@@ -19,10 +23,13 @@ public class CreateTransaction {
   }
 
   public CreateTransactionResult execute(CreateTransactionRequest request) {
-    return terminalFinder.findBy(request.terminalId(), request.terminalThreatScore())
+    var terminalId = request.terminalId();
+    LOGGER.info("Attempt to create transaction for terminal {}", terminalId);
+
+    return terminalFinder.findBy(terminalId, request.terminalThreatScore())
         .map(createTransaction(request))
         .orElseThrow(
-            () -> new NotFoundServiceException("Unknown terminal " + request.terminalId()));
+            () -> new NotFoundServiceException("Unknown terminal " + terminalId));
   }
 
   private Function<Terminal, CreateTransactionResult> createTransaction(
@@ -30,10 +37,13 @@ public class CreateTransaction {
     return terminal -> {
       var transaction = transactionService.createTransaction(terminal, request.amount(),
           request.cardNumber());
-      return new CreateTransactionResult(
+
+      var createTransactionResult = new CreateTransactionResult(
           transaction.getStatus(),
           transaction.getMessage(),
           transaction.getFraudScore());
+      LOGGER.info("Created transaction with following result {}", createTransactionResult);
+      return createTransactionResult;
     };
   }
 
